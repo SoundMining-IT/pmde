d26ae130b0ff858c53b712d6b82485edee3b920f
Dewald van Tonder <dewaldvantonder2@gmail.com>
2025-07-01 14:18:39 +0200

    meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta
+          name="google-site-verification"
+          content="WRpytFvrLz4i-l5MqhUOqcQA5iHnra2kzzTL_zRQ8PY"
+        />
         {/* <script
           src="https://www.bugherd.com/sidebarv2.js?apikey=pfxhlfgixlgcrpxbbndoha"
           async
